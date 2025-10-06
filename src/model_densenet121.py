942e759d091ed5f1ea41418e513e52fe299eb00d
Pradicksha <pradicksha1230@gmail.com>
2025-10-06 16:53:12 +0530

    Densenet121 - directory changes made

--- src/model_densenet121.py
+++
@@ -1,8 +0,0 @@
-import timm
-import torch.nn as nn
-
-def build_densenet121(num_classes: int, pretrained: bool = False):
-    model = timm.create_model('densenet121', pretrained=pretrained)
-    in_features = model.get_classifier().in_features
-    model.classifier = nn.Linear(in_features, num_classes)
-    return model
